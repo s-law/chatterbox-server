a77c26ad4e75f6db743eb14a26fa36a00316d697
s-law <slaw@nuclearanvil.com>
2015-12-14 16:50:02 -0800

    Created handler logic to specify actions depending upon route (provided by request.url.pathname) and action type.

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,6 +11,11 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var url = require('url');
+
+var dataStore = {
+  results: []
+}
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -28,9 +33,10 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log("Serving request type " + request.method + " for url " + request.url);
+  console.log(request.body);
 
   // The outgoing status.
-  var statusCode = 200;
+  // var statusCode = 200;
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
@@ -39,11 +45,34 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "application/JSON";
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  var parsed = url.parse(request.url, true);
+  var route = parsed.pathname
+  if (route === '/classes/messages') {
+    if (request.method === 'GET') {
+      response.writeHead(200, headers);
+      response.end(JSON.stringify(dataStore));
+    } else if (request.method === 'POST') {
+      var dataHolder = '';
+      request.on('data', function(data) {
+        dataHolder += data;
+      });
+
+      request.on('end', function() {
+        dataStore.results.push(JSON.parse(dataHolder));
+        console.log(dataStore.results);
+        response.writeHead(201, headers);
+        response.end();  
+      });
+    }
+  } else {
+    response.writeHead(404, headers);
+    response.end();
+  }
+
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +81,6 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -71,3 +99,4 @@ var defaultCorsHeaders = {
   "access-control-max-age": 10 // Seconds.
 };
 
+module.exports = requestHandler;
