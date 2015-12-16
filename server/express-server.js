12f85adc7db5e62ab2e1d2a672f6b290ed7ca76d
s-law <slaw@nuclearanvil.com>
2015-12-15 19:58:53 -0800

    Added multi-core support.

--- server/express-server.js
+++ server/express-server.js
@@ -1,46 +1,56 @@
-var express = require('express');
-// var cors = require('cors');
-var app = express();
-var fs = require('fs');
-var path = require('path');
+var cluster = require('cluster');
 
-var dataStore = {
-  results: []
-};
+if (cluster.isMaster) {
+  var numCPUs = require('os').cpus().length;
 
-var logPath = path.join(__dirname, './log.txt');
-
-fs.readFile(logPath, function(err, data) {
-  if (err) {
-    throw err;
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-  var dataToBeLoaded = data.toString().split('\n');
-  for (var i = 0; i < dataToBeLoaded.length; i++) {
-    dataToBeLoaded[i] = JSON.parse(dataToBeLoaded[i]);
-  }
-  dataStore.results = dataToBeLoaded;
-});
+} else {
+  var express = require('express');
+  // var cors = require('cors');
+  var app = express();
+  var fs = require('fs');
+  var path = require('path');
 
-app.use(express.static(__dirname + '/client'));
+  var dataStore = {
+    results: []
+  };
 
-app.route('/classes/messages')
-.get(function(req, res) {
-  res.send(JSON.stringify(dataStore));
-})
-.post(function(req, res) {
-  var dataHolder = '';
-  req.on('data', function(data) {
-    dataHolder += data;
-  }).on('end', function() {
-    dataStore.results.push(JSON.parse(dataHolder));
-    fs.appendFile(logPath, '\n' + dataHolder, function (err) {});
-    res.sendStatus(201);
+  var logPath = path.join(__dirname, './log.txt');
+
+  fs.readFile(logPath, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    var dataToBeLoaded = data.toString().split('\n');
+    for (var i = 0; i < dataToBeLoaded.length; i++) {
+      dataToBeLoaded[i] = JSON.parse(dataToBeLoaded[i]);
+    }
+    dataStore.results = dataToBeLoaded;
   });
-});
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+  app.use(express.static(__dirname + '/client'));
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+  app.route('/classes/messages')
+  .get(function(req, res) {
+    res.send(JSON.stringify(dataStore));
+  })
+  .post(function(req, res) {
+    var dataHolder = '';
+    req.on('data', function(data) {
+      dataHolder += data;
+    }).on('end', function() {
+      dataStore.results.push(JSON.parse(dataHolder));
+      fs.appendFile(logPath, '\n' + dataHolder, function (err) {});
+      res.sendStatus(201);
+    });
+  });
+
+  var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+  });
+}
