18a78ecc3a54c536701c3e91c68d6a782c3e2a94
s-law <slaw@nuclearanvil.com>
2015-12-15 17:11:22 -0800

    Refactored server using express.

---
+++ server/express-server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+// var cors = require('cors');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+
+var dataStore = {
+  results: []
+};
+
+var logPath = path.join(__dirname, './log.txt');
+
+fs.readFile(logPath, function(err, data) {
+  if (err) {
+    throw err;
+  }
+  var dataToBeLoaded = data.toString().split('\n');
+  for (var i = 0; i < dataToBeLoaded.length; i++) {
+    dataToBeLoaded[i] = JSON.parse(dataToBeLoaded[i]);
+  }
+  dataStore.results = dataToBeLoaded;
+});
+
+app.use(express.static(__dirname + '/client'));
+
+app.get('/classes/messages', function (req, res) {
+  res.send(JSON.stringify(dataStore));
+});
+
+app.post('/classes/messages', function (req, res) {
+  var dataHolder = '';
+  req.on('data', function(data) {
+    dataHolder += data;
+  }).on('end', function() {
+    dataStore.results.push(JSON.parse(dataHolder));
+    fs.appendFile(logPath, '\n' + dataHolder, function (err) {});
+    res.sendStatus(201);
+  });
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
